d58d212a64ac2681b094c88753c0537c4288b85e
Zaitis <krzysztof@painm.pl>
2023-03-19 11:38:30 +0100

    inital commit

---
+++ src/test/java/pl/zaitis/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zaitis.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
